2c24057c03bac1354136b46ddf1c9e29812aa225
MrCrankHank <MrCrankHank@users.noreply.github.com>
2014-11-11 21:32:51 +0100

    Update lvm_system_backup.sh

--- lvm_system_backup.sh
+++ lvm_system_backup.sh
@@ -5,7 +5,9 @@
 
 # Define default vars
 hostname=$(</etc/hostname)
+LOCKFILE=/var/run/lvm_system_backup.lock
 
+# Look for config in /etc/default
 if [ -f /etc/default/lvm_system_backup ]; then
         . /etc/default/lvm_system_backup
 else
@@ -24,12 +26,14 @@ else
 	fi
 fi
 
+# Check if $BACKUP_BOOT var is set to 0/1
 if [ -z $BACKUP_BOOT ]; then
 	echo "BACKUP_BOOT is not configured!"
 	echo "Please check the config file!"
 	exit 1
 fi
 
+# Check if all vars from the config file are configured
 if [ $BACKUP_BOOT == 1 ]; then
 	if [[ -z "$VG_NAME" && -z "$DIR" && -z "$HOST" && -z "$USER" && -z "$DISK" && -z "$BOOT" ]]; then
 		echo "Important vars are missing!"
@@ -44,6 +48,7 @@ else
 	fi
 fi
 
+# Check if the specified volume group is there
 if [ -d /dev/$VG_NAME ]; then
 	true
 else
@@ -56,30 +61,37 @@ datum=`date +%d.%m.%y`
 time=`date +"%T"`
 DIR=$DIR/$hostname/$datum
 
+# Create lock file
+touch $LOCKFILE
+
 # Create list with logical volumes
 lvdisplay $VG_NAME | grep -e "LV Name" | tr -d ' ' | sed -e 's/LVName//g' > /tmp/lvs
 
-# Exit trap to delete the snapshots, if the script terminates to early
+# Exit trap to delete the snapshots and the lockfile, if the script terminates to early
 function finish {
         while read lv; do
                 if [ -e /dev/$VG_NAME/${lv}_snap ]; then
                         lvremove -f /dev/$VG_NAME/${lv}_snap
                 fi
         done < /tmp/lvs
+        rm $LOCKFILE
 }
 trap finish EXIT
 
 function backup_layout {
+	# Backup partition table
 	sfdisk -d $DISK > /tmp/part_table
 	scp /tmp/part_table ${USER}@$HOST:$DIR/part_table
 	rm /tmp/part_table
 
+	# Backup lvm structure
 	vgcfgbackup -f /tmp/lvm_structure /dev/$VG_NAME
 	scp /tmp/lvm_structure ${USER}@$HOST:$DIR/lvm_structure
 	rm /tmp/lvm_structure
 }
 
 function backup_lvs {
+	# Create a snapshot of every volume and copy it using dd
 	while read lv; do
 		lvcreate --snapshot -L $SIZE -n ${lv}_snap /dev/$VG_NAME/$lv
 		dd if=/dev/$VG_NAME/${lv}_snap | gzip -1 - | ssh ${USER}@$HOST dd of=$DIR/${lv}.img.gz
@@ -103,3 +115,6 @@ fi
 
 # Backup the logical volumes
 backup_lvs
+
+# Delete lock file
+rm $LOCKFILE
