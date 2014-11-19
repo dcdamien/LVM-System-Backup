258dd3e89f148afdb471607022d8c911362e4b74
MrCrankHank <MrCrankHank@users.noreply.github.com>
2014-11-19 14:56:57 +0100

    Update lvm_system_backup.sh

--- lvm_system_backup.sh
+++ lvm_system_backup.sh
@@ -36,6 +36,13 @@ fi
 if [ -f /etc/default/lvm_system_backup_config ]; then
 	. /etc/default/lvm_system_backup_config
 	log_verbose "${ORANGE}Verbose: ${NC}Found the config file under /etc/default/lvm_system_backup_config"
+	if [ -z $1 ]; then
+		if [ -f $1 ]; then
+			log_verbose "${ORANGE}Verbose: ${NC}Ignoring file at ${1}, because config file was found at default location"
+		else
+			log_verbose "${ORANGE}Verbose: ${NC}Ignoring file at ${1}, because it wasn't found. Since the config file was found
+		fi
+	fi	
 else
 	if [ -z $1 ]; then
 		log_error "${RED}Error: ${NC}Can't find the config file at default location"
