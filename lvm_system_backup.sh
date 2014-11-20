9b9e23931ae8e081883d5e13907eab699ba288ad
MrCrankHank <MrCrankHank@users.noreply.github.com>
2014-11-20 21:20:36 +0100

    Update lvm_system_backup.sh

--- lvm_system_backup.sh
+++ lvm_system_backup.sh
@@ -259,7 +259,7 @@ function samba_backup {
 				
 				log_verbose "${ORANGE}Verbose: ${NC}Copy ${SAMBA_DIRS[$COUNTER]} to /tmp/samba"
 				mkdir -p /tmp/samba/${SAMBA_DIRS[$COUNTER]} &>/dev/null
-				rsync -avzqR --exclude ${SAMBA_DIRS[$COUNTER]}/*.ldb ${SAMBA_DIRS[$COUNTER]} /tmp/samba
+				rsync -avzqR --exclude *.ldb ${SAMBA_DIRS[$COUNTER]} /tmp/samba
 				if [ $? -ne 0 ]; then
 					log_error "${RED}Error: ${NC}Couldn't rsync ${SAMBA_DIRS[$COUNTER]} to /tmp/samba"
 					exit 1
