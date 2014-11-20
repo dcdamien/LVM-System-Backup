8f1b370640d7207bf4b1f02feb494b19f6c194e3
MrCrankHank <MrCrankHank@users.noreply.github.com>
2014-11-20 20:53:39 +0100

    Update lvm_system_backup.sh

--- lvm_system_backup.sh
+++ lvm_system_backup.sh
@@ -280,7 +280,7 @@ function samba_backup {
 		fi
 				
 		log_verbose "${ORANGE}Verbose: ${NC}Sending /tmp/samba4.tar.gz to $HOST"
-		scp /tmp/samba4.tar.gz ${USER}@$HOST:$DIR/samba4.tar.gz
+		scp /tmp/samba4.tar.gz ${USER}@$HOST:$DIR/samba4.tar.gz &>/dev/null
 		if [ $? -ne 0 ]; then
 			log_error "${RED}Error: ${NC}Error while sending /tmp/samba4.tar.gz to $HOST"
 		fi
